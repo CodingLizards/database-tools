40a8b49a8df9ef780f92b414b06f0766a277d7ab
Imanuel Ulbricht <imanuel.ulbricht@outlook.com>
2016-09-14 19:41:54 +0200

    Projektdateien hinzufügen.

---
+++ src/Coding.Lizards.Database.Creation/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Coding.Lizards.Database.Creation
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+    }
+}
